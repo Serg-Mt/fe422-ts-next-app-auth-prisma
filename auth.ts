f27f8a85c44fe3c99e5d3480fbbaefded8794b46
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2025-02-09 17:38:51 +0400

    fix: update Vk provider to include checks in configuration

--- auth.ts
+++ auth.ts
@@ -17,7 +17,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHub,
     Google,
-    Vk({ clientId: process.env.AUTH_VK_ID, clientSecret: process.env.AUTH_VK_SECRET }),
+    Vk({ clientId: process.env.AUTH_VK_ID, clientSecret: process.env.AUTH_VK_SECRET,checks:['none'] }),
 
     CredentialsProvider({
       // The name to display on the sign in form (e.g. 'Sign in with...')
